d4830dcc0471bdc316442adfe41cd02b7253c537
rafael loiola dos santos <manga_loiola@hotmail.com>
2025-03-27 10:05:02 -0300

    aula if else

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const nota1 = 3;
 const nota2 = 9;
 const nota3 = 9;
 const media = (nota1 + nota2 + nota3) /3;
-console.log('media semestre: '+ media)
+console.log('1) media semestre: '+ media)
 
 if (media < 5 ) {
 	console.log ('reprovado');
@@ -34,4 +34,33 @@ if (media < 5 ) {
 	console.log ('recuperação')
 } else {
 	console.log ('passou semestre')
+}
+// 2) O IMC - Indice de Massa Corporal é um criterio da Organização Mundial de Saude para dar uma indicação sobre a condição de peso de uma pessoa adulta.
+
+// Formula do IMC:
+// IMC = peso / ( altura * altura)
+
+// Elabore um algoritimo que dado o peso e a altura de um adulto mostre sua condição de acordo com a tabela abaixo.
+
+// IMC em adultos Condição :	
+// - Abaixo de 18.5, Abaixo do Peso ;
+// - Entre 18.5 e 25, Peso Normal;
+// - Entre 25 e 30,  Acima do Peso;
+// - Entre 30 e 40, Obeso;
+// - Acima de 40 Obesidade Grave;
+const peso = 150;
+const altura = 1.83;
+const imc = peso / Math.pow(altura,2);
+console.log ('2) IMC:'+ imc.toFixed(2))
+
+if (imc < 18.5){
+	console.log ('Condição:abaixo do peso');
+} else if (imc >= 18.5 && imc < 25 ){
+	console.log ('Condição:Peso Normal');
+}else if (imc >=25 && imc < 30 ){
+	console.log ('Condição:acima do Peso');
+}else if (imc >= 30 && imc < 40){
+	console.log ('Obeso');
+} else {
+	console.log ('Condição:Obesidade Grave')
 }
